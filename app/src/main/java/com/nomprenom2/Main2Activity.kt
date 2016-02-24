9ac70a09d3a1d5da88af1bb89615f7ddb93a3ace
mike <usachev.mikhail@gmail.com>
2016-02-24 23:45:14 +0400

    MOB-9 : Android extensions : initial

--- app/src/main/java/com/nomprenom2/Main2Activity.kt
+++ app/src/main/java/com/nomprenom2/Main2Activity.kt
@@ -1,21 +1,21 @@
 package com.nomprenom2
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 
-class Main2Activity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main2.*
 
+class Main2Activity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val toolbar = findViewById(R.id.toolbar) as Toolbar
-        setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
     }
 
 }
