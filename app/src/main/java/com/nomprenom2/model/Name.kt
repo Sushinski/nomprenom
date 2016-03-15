7ddbf7c06b85d490aa14b26b82a72a780562d9c7
mike <usachev.mikhail@gmail.com>
2016-03-15 22:06:02 +0400

    MOB-35: Name -> open class

--- app/src/main/java/com/nomprenom2/model/Name.kt
+++ app/src/main/java/com/nomprenom2/model/Name.kt
@@ -2,14 +2,22 @@ package com.nomprenom2.model
 
 import io.realm.RealmObject
 import io.realm.annotations.Ignore
+import io.realm.annotations.PrimaryKey
 
 /**
  * Created by musachev on 01.03.2016.
  */
-class Name(var name: String? = "unknown") : RealmObject()
+/* Your model has to extend RealmObject.
+   Class and all of the properties must be annotated with 'open' (as classes and functions are final by default).
+   You can put properties in the constructor as long as all of them are initialized with
+   default values. This ensures that an empty constructor is generated.
+   All properties are persisted by default. Properties can be annotated with PrimaryKey or Index.
+*/
+open class Name(@PrimaryKey open var name: String? = "unknown") : RealmObject()
 {
-    var nickname = name
-    var group_id: Int? = null
+    open var nickname = name
+    open var group_id: Int? = null
     @Ignore
-    var tempRef : Name? = null
+    open var tempRef : Name? = null
+    open var id: Long = 0
 }
